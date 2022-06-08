2b62a21b177dfa9e5b21adf4e5b25b8cc413840a
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-06-07 22:17:25 -0300

    Update textocor.py

--- textocor.py
+++ textocor.py
@@ -1,16 +1,16 @@
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 response = openai.Completion.create(
   engine="text-davinci-002",
-  prompt="You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
-  temperature=0.5,
+  prompt="Correct this to standard English:\n\nI tell you seven years ago.",
+  temperature=0,
   max_tokens=60,
   top_p=1.0,
-  frequency_penalty=0.5,
-  presence_penalty=0.0,
-  stop=["You:"]
+  frequency_penalty=0.0,
+  presence_penalty=0.0
 )
+
 print(response)
